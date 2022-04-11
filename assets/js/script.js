a1898ce829910d9a462094bf2e4c3f93816b1d1e
cam-cole <camcole065@gmail.com>
2022-04-10 21:35:25 -0500

    working on formatting index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,8 @@
 var todayDate = moment().format('dddd, MMMM Do')
+var currentTime = moment().format('h:mm a');
 
 // Displaying the current date
 $("#currentDay").text(todayDate);
+
+// Change textarea color based on time
+console.log(currentTime);
